2c490488ffeb9c93aba97b380a1891df0906ac3a
Daniel Hornik <daniel.1302@gmail.com>
2017-12-10 15:36:36 +0100

    Matrix arithmetic with another matrix

--- Math/Matrix.h
+++ Math/Matrix.h
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <exception>
+
 
 using namespace std;
 
@@ -17,10 +19,11 @@ private:
 public:
     Matrix();
     ~Matrix();
-    void insert(uint32_t x, uint32_t y, T value);
-    string print();
-    int cols();
-    int rows();
+    void insert(uint32_t row, uint32_t col, T value);
+    string print() const;
+    uint32_t cols() const;
+    uint32_t rows() const;
+    T get(uint32_t row, uint32_t col) const;
 
 public:
 //    Matrix<T> operator*(const double multiplier);
@@ -45,7 +48,7 @@ Matrix<T>::~Matrix()
 }
 
 template <typename T>
-int Matrix<T>::cols()
+uint32_t Matrix<T>::cols() const
 {
     if (_matrix.size() > 0) {
         return _matrix[0].size();
@@ -55,7 +58,7 @@ int Matrix<T>::cols()
 }
 
 template <typename T>
-int Matrix<T>::rows()
+uint32_t Matrix<T>::rows() const
 {
     return _matrix.size();
 }
@@ -83,7 +86,7 @@ void Matrix<T>::insert(uint32_t row, uint32_t col, T value)
 }
 
 template <typename T>
-string Matrix<T>::print()
+string Matrix<T>::print() const
 {
     stringstream out;
 
@@ -100,14 +103,53 @@ string Matrix<T>::print()
 
     return out.str();
 }
+//
+//template <typename T>
+//Matrix<T> Matrix<T>::operator*(const double multiplier)
+//{
+//    this->multiply(multiplier);
+//
+//    return Matrix<T>(*this);
+//}
 
 template <typename T>
-Matrix<T> Matrix<T>::operator*(const double multiplier)
+Matrix<T> Matrix<T>::operator*=(const Matrix<T> matrix)
 {
-    this->multiply(multiplier);
+    vector< vector<T> > tmpData(_matrix);
+    uint32_t rows = this->rows();
+    uint32_t cols = this->cols();
 
-    return Matrix<T>(*this);
+
+    if (this->cols() != matrix.rows() || this->rows() != matrix.cols()) {
+        throw logic_error("Cannot multiply matrixes with different sizes");
+    }
+
+    T tmpValue;
+
+    for (uint32_t row1=0; row1<rows; row1++) {
+        for (uint32_t col2=0; col2<matrix.cols(); col2++) {
+
+            tmpValue = 0;
+
+            for (uint32_t row2=0; row2<matrix.rows(); row2++) {
+                for (uint32_t col1=0; col1<cols; col1++) {
+                    tmpValue += tmpData[row1][col1] * matrix.get(row2, col2);
+                }
+            }
+
+            this->insert(row1, col2, tmpValue);
+        }
+    }
+
+    return *this;
 }
+
+template <typename T>
+T Matrix<T>::get(uint32_t row, uint32_t col) const
+{
+    return _matrix[row][col];
+}
+
 
 template <typename T>
 Matrix<T> Matrix<T>::operator*=(const int multiplier)
