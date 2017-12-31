0583f8327e3f388fe032ac5b296acdfca9ca05cb
Daniel Hornik <daniel.1302@gmail.com>
2017-12-31 19:47:59 +0100

    Added sample circuit

--- main.cpp
+++ main.cpp
@@ -5,13 +5,39 @@
 
 #ifdef TEST
     #include <Tests/Matrix.cpp>
+#include <Circuit/PowerSupply.h>
+#include <Circuit/ResistorElement.h>
+
 #endif
 
 
+void testCircuit()
+{
+    Circuit circuit;
+    std::cout<<circuit.print();
 
+    PowerSupply current1(0, 1, 0.001);
+    ResistorElement r1(1, 2, 10000);
+    ResistorElement r2(2, 0, 20000);
+    ResistorElement r3(2, 3, 16000);
+    ResistorElement r4(3, 0, 4000);
+
+    circuit.insertElement((Element*)&current1);
+    circuit.insertElement((Element*)&r1);
+    circuit.insertElement((Element*)&r2);
+    circuit.insertElement((Element*)&r3);
+    circuit.insertElement((Element*)&r4);
+
+
+
+
+    std::cout<<circuit.print();
+}
 
 int main() {
-    matrixTest();
+//    matrixTest();
+    testCircuit();
+
     return 0;
 }
 
